b617a61f994277deec48ce3241db7c60b11de289
Max-Mtzz <20243ds119@utez.edu.mx>
2025-08-09 17:28:43 -0600

    declare las variables de las columnas de mi tabla

--- src/main/java/com/jmc/gestor/HelloController.java
+++ src/main/java/com/jmc/gestor/HelloController.java
@@ -8,11 +8,10 @@ import javafx.scene.control.*;
 import java.time.LocalDate;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    @FXML private TableView<Tarea> tablaTareas;
+    @FXML private TableColumn<Tarea, Integer> colId;
+    @FXML private TableColumn<Tarea, String> colTitulo;
+    @FXML private TableColumn<Tarea, String> colDescripcion;
+    @FXML private TableColumn<Tarea, String> colPrioridad;
+    @FXML private TableColumn<Tarea, LocalDate> colFechaLimite;
 }
