10aa24363a703bc3693cbe2273aa0ca7a2301652
Max-Mtzz <20243ds119@utez.edu.mx>
2025-08-09 17:23:42 -0600

    Cree el proyecto con el diseño y el css incluidos junto con la clase tarea y sus constructores

---
+++ src/main/java/com/jmc/gestor/Tarea.java
@@ -0,0 +1,59 @@
+package com.jmc.gestor;
+
+import java.time.LocalDate;
+
+public class Tarea {
+    private int id;
+    private String titulo;
+    private String descripcion;
+    private String prioridad;
+    private LocalDate fechaLimite;
+
+    public Tarea(int id, String titulo, String descripcion, String prioridad, LocalDate fechaLimite) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+        this.fechaLimite = fechaLimite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public LocalDate getFechaLimite() {
+        return fechaLimite;
+    }
+
+    public void setFechaLimite(LocalDate fechaLimite) {
+        this.fechaLimite = fechaLimite;
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.jmc.gestor {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.jmc.gestor to javafx.fxml;
+    exports com.jmc.gestor;
+}
